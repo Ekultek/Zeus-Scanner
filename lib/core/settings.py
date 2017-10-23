a06371aa72acc94579d466a75a138fe47cf3a79a
ekultek <thomas.perkins23@icloud.com>
2017-10-23 14:11:03 -0500

    fixes an issue with unicode errors from XSS payloads (issue #124)

--- lib/core/settings.py
+++ lib/core/settings.py
@@ -32,7 +32,7 @@ PATCH_ID = str(subprocess.check_output(["git", "rev-parse", "origin/master"]))[:
 # clone link
 CLONE = "https://github.com/ekultek/zeus-scanner.git"
 # current version <major.minor.commit.patch ID>
-VERSION = "1.0.58".format(PATCH_ID)
+VERSION = "1.0.58.{}".format(PATCH_ID)
 # colors to output depending on the version
 VERSION_TYPE_COLORS = {"dev": 33, "stable": 92, "other": 30}
 # version string formatting
