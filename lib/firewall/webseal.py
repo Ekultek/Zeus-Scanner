9cb82a70968d4bd42a22ce443dfd7f1b907d40a3
ekultek <thomas.perkins23@icloud.com>
2017-11-13 20:43:41 -0600

    minor update to webseal WAF script, will better recognize now

--- lib/firewall/webseal.py
+++ lib/firewall/webseal.py
@@ -1,13 +1,13 @@
 import re
 
 
-__item__ = "IBM Security Access Manager WebSEAL"
+__item__ = "IBM Security Access Manager (WebSEAL)"
 
 
 def detect(content, **kwargs):
     content = str(content)
     detection_schema = (
-        re.compile(r"\bWebSEAL\b"), re.compile(r"\bIBM\b")
+        re.compile(r"\bWebSEAL\b", re.I), re.compile(r"\bIBM\b", re.I)
     )
     for detection in list(detection_schema):
         if detection.search(content) is not None:
