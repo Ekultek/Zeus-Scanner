ae134c3989415f6bb28b80c6945a435879bf1f5c
ekultek <thomas.perkins23@icloud.com>
2018-02-07 11:28:02 -0600

    removes the old profile of firefox from /usr/lib now (issue #353)

--- etc/scripts/reinstall.sh
+++ etc/scripts/reinstall.sh
@@ -2,6 +2,7 @@
 
 for pid in $(ps -ef | grep "firefox" | awk '{print $2}'); do kill -9 ${pid}; done > /dev/null 2>&1
 mv ~/.mozilla ~/.mozilla.old > /dev/null 2>&1
+rm /usr/lib/firefox* > /dev/null 2>&1
 sudo apt-get update > /dev/null 2>&1
 sudo apt-get --purge --reinstall --assume-yes install firefox=56.0 > /dev/null 2>&1
-sudo pip2 install selenium -U
+sudo pip2 install selenium -U > /dev/null 2>&1
